8bc3f5c3fa790ecbfd7db23d2786da2a9ac16dc7
enZane <ezequiel@octahedroid.com>
2023-10-04 15:36:45 -0600

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import tailwind from "@astrojs/tailwind";
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: 'us', // optional,  or 'eu' (default)
+      },
+    }),
+    tailwind()
+  ],
+});
