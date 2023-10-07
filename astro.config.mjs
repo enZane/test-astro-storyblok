779d76445162d164810cb85737a2b100e76b518f
enZane <ezequiel@octahedroid.com>
2023-10-06 19:00:37 -0600

    :sparkles: Add preview support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,24 @@ import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        hero: 'storyblok/hero',
-        Page: 'storyblok/page',
-      },
-      apiOptions: {
-        // Choose your Storyblok space region
-        region: 'us', // optional,  or 'eu' (default)
-      },
-    }),
-    tailwind()
-  ],
+  integrations: [storyblok({
+    bridge: process.env.PUBLIC_ENV !== 'production',
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      hero: 'storyblok/hero',
+      Page: 'storyblok/page'
+    },
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: 'us' // optional,  or 'eu' (default)
+    }
+  }), tailwind()],
+  output: process.env.PUBLIC_ENV === 'preview' ? 'server' : 'static',
+  adapter: process.env.PUBLIC_ENV === 'preview' ? vercel() : undefined,
 });
